b5fecc719ef611a59692588f9f6e16e331206d5d
Beats <daniel15042014@hotmail.com>
2022-10-04 23:41:14 -0400

    clear

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,30 +1,20 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Threading;
 using System.IO;
 using System.IO.Compression;
 using System.Net;
 using System.Diagnostics;
-using System.Windows.Threading;
 
 namespace CanaryLauncherUpdate
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
     {
 		WebClient webClient = new WebClient();
 		bool clientDownloaded = false;
